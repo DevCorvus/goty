12f186f3590b68a1e81eaefbece6dd7e3ce6477d
Luis Portillo <devcorvus.contact@gmail.com>
2022-10-16 19:58:11 -0400

    feat: serving static files, some html pages and handlers (register/login)

--- server.go
+++ server.go
@@ -5,12 +5,48 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
+var static = "./static"
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, filepath.Join(static, "/register.html"))
+
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		// TODO: Handle data properly
+		fmt.Fprintf(w, "%v", r.PostForm)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, filepath.Join(static, "/login.html"))
+
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		// TODO: Handle data properly
+		fmt.Fprintf(w, "%v", r.PostForm)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
 func setupHandlers(mux *http.ServeMux) {
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Game of the year")
-	})
+	mux.HandleFunc("/register", registerHandler)
+	mux.HandleFunc("/login", loginHandler)
 
 	mux.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -21,6 +57,9 @@ func main() {
 	listenAddr := getListenAddr()
 
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir(static))
+	mux.Handle("/", fileServer)
 
 	setupHandlers(mux)
 
