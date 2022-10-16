4558d2a5153cb67363effeef52f0e2b6f8517ed7
Luis Portillo <devcorvus.contact@gmail.com>
2022-10-16 18:39:40 -0400

    feat: new multiplexer and configurable port

--- main.go
+++ main.go
@@ -4,19 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	listenAddr := ":"
+
+	port := os.Getenv("PORT")
+	if len(port) > 0 {
+		listenAddr += port
+	} else {
+		listenAddr += "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Game of the year")
 	})
 
-	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	fmt.Println("Listening...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Printf("Listening... http://localhost%s\n", listenAddr)
+	if err := http.ListenAndServe(listenAddr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
